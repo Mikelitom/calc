fbee9e49ee0bbcea3c1068bd87fc6cb542653387
Miguel Fajardo <majfe202@hotmail.com>
2025-12-14 22:48:03 -0700

    core -> declaracion de entidades Token.h y TokenType.h

--- src/core/Token.h
+++ src/core/Token.h
@@ -3,5 +3,11 @@
 #include "TokenType.h"
 
 struct Token {
+    TokenType type;
+    std::string lexeme;
+    double numericValue;
+    size_t position;
 
-}
+    Token(TokenType t, const std::string l, size_t pos)
+        : type(t), lexeme(l), numericValue(0.0), position(pos) {}
+};
